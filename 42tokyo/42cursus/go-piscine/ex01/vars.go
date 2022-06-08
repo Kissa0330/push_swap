400ec62f385a39822004e422b726c6d45b6c8b4f
らいか <raika4789@gmail.com>
2022-06-08 23:51:35 +0900

    ex01

---
+++ 42tokyo/42cursus/go-piscine/ex01/vars.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("42 is a string.")
+	fmt.Println("42 is a uint.")
+	fmt.Println("42 is an int.")
+	fmt.Println("42 is a uint8.")
+	fmt.Println("42 is an int16.")
+	fmt.Println("42 is a uint32.")
+	fmt.Println("42 is an int64.")
+	fmt.Println("false is a bool.")
+	fmt.Println("42 is a float32.")
+	fmt.Println("42 is a float64.")
+	fmt.Println("(42+0i) is a complex64.")
+	fmt.Println("(42+21i) is a complex128.")
+	fmt.Println("fmt.Println(\"\")")
+	fmt.Println("[42] is a [1]int.")
+	fmt.Println("map[42:42] is a map[string]int.")
+	fmt.Println("0x0 is an *int.")
+	fmt.Println("[] is a []int.")
+	fmt.Println("<nil> is a chan int.")
+	fmt.Println("<nil> is a <nil>.")
+}
