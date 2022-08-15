c99c9f64243ac8ded49650b68677bfa30670826e
らいか <raika4789@gmail.com>
2022-08-16 02:22:26 +0900

    原型

--- 42tokyo/42cursus/push_swap/push_swap.c
+++ 42tokyo/42cursus/push_swap/push_swap.c
@@ -1,7 +1,13 @@
 #include "push_swap.h"
 
-int push_swap(int num,...)
+int push_swap(char *list_origin[])
 {
-	va_list nums;
+	t_lists lists;
 
+	if(list_origin[0] == NULL)
+		return (0);
+	if(list_check(list_origin))
+		write("Error\n");
+	atoi_list(list_origin, &lists);
+	swap(lists);
 }
--- 42tokyo/42cursus/push_swap/push_swap.h
+++ 42tokyo/42cursus/push_swap/push_swap.h
@@ -1,5 +1,10 @@
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
-#include "stdarg.h"
-
+	#include "stddef.h"
+	typedef struct s_lists
+	{
+		int *a;
+		int *b;
+	}	t_lists;
+	
 #endif
