121eaef2a4303594fdf15381dba5f3bce3ddc88e
raika <62830507+Kissa0330@users.noreply.github.com>
2022-08-19 01:18:14 +0900

    Delete atcoder directory

--- atcoder/2021:3:27_a.js
+++
@@ -1,6 +0,0 @@
-function main(input) {
-  let inputSplit = input.split("");
-  let inputAfter = inputSplit[1] + inputSplit[2] + inputSplit[0];
-  console.log(inputAfter);
-}
-main(require("fs").readFileSync("/dev/stdin", "utf8"));
