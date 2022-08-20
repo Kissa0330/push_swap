d2197acc0f0a9e54041aad34a65c0bb54cee6ea1
らいか <raika4789@gmail.com>
2022-08-20 17:51:21 +0900

    update header

--- push_swap.h
+++ push_swap.h
@@ -17,4 +17,15 @@ void		ft_bzero(void *s, size_t n);
 int			*swap(int *list);
 int			*push(int *list1, int *list2);
 int			*rotate(int mod, int *list, size_t length);
+void		command_sa(t_lists lists);
+void		command_sb(t_lists lists);
+void		command_ss(t_lists lists);
+void		command_pa(t_lists lists);
+void		command_pb(t_lists lists);
+void		command_ra(t_lists lists, size_t length);
+void		command_rb(t_lists lists, size_t length);
+void		command_rr(t_lists lists, size_t length);
+void		command_rra(t_lists lists, size_t length);
+void		command_rrb(t_lists lists, size_t length);
+void		command_rrr(t_lists lists, size_t length);
 #endif
